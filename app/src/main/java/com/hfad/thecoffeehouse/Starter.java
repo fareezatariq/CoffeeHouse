65b9e48ec30962f9932e8595fe0d19cb93d04b9e
fareezatariq <fareezatariq@gmail.com>
2022-10-04 16:43:00 -0700

    connected array to list view

--- app/src/main/java/com/hfad/thecoffeehouse/Starter.java
+++ app/src/main/java/com/hfad/thecoffeehouse/Starter.java
@@ -1,13 +1,20 @@
 package com.hfad.thecoffeehouse;
 
+import androidx.annotation.NonNull;
+
 public class Starter {
     private String name;
     private String description;
     private int imageID;
-    private static final Starter[] starters={
-      new Starter("Mozzarella Sticks", "Filled with creamy mozzarella cheese and coated with delicious spices", R.drawable.mozzarella),
-            new Starter("Nuggets", "Juicy and tendered chicken coated with delicious spices", R.drawable.nuggets),
-            new Starter("Chicken Salad","Mix of fresh vegetables and juicy chicken for a healthy start", R.drawable.chickensalad )
+    public static final Starter[] starters={
+      new Starter("Mozzarella Sticks",
+              "Filled with creamy mozzarella cheese " +
+                      "and coated with delicious spices", R.drawable.mozzarella),
+            new Starter("Nuggets", "Juicy and tendered chicken " +
+                    "coated with delicious spices", R.drawable.nuggets),
+            new Starter("Chicken Salad","Mix of fresh " +
+                    "vegetables and juicy chicken for " +
+                    "a healthy start", R.drawable.chickensalad )
     };
   private Starter  (String name, String description, int imageID){
       this.name=name;
@@ -18,4 +25,8 @@ public class Starter {
     public String getName() {return name;}
     public String getDescription() {return description;}
     public int getImageID() {return imageID;}
+    @NonNull
+    public String toString() {
+        return this.name;
+    }
 }
--- app/src/main/java/com/hfad/thecoffeehouse/StarterOptions.java
+++ app/src/main/java/com/hfad/thecoffeehouse/StarterOptions.java
@@ -3,6 +3,9 @@ package com.hfad.thecoffeehouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class StarterOptions extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class StarterOptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter_options);
+        ArrayAdapter<Starter> listAdapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Starter.starters);
+        ListView listStart= (ListView) findViewById(R.id.list_starters);
+        listStart.setAdapter(listAdapter);
+
     }
 }
