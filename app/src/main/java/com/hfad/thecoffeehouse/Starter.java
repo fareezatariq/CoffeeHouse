66cbed6cf8d475c444115b01bdd3f246f6994066
fareezatariq <fareezatariq@gmail.com>
2022-10-04 16:27:20 -0700

    Added starters class

---
+++ app/src/main/java/com/hfad/thecoffeehouse/Starter.java
@@ -0,0 +1,23 @@
+package com.hfad.thecoffeehouse;
+
+public class Starter {
+    private String name;
+    private String description;
+    private int imageID;
+    private static final Starter[] starters={
+      new Starter("Mozzarella Sticks", "Filled with creamy mozzarella cheese and coated with delicious spices", R.drawable.mozzarella),
+            new Starter("Nuggets", "Juicy and tendered chicken coated with delicious spices", R.drawable.nuggets),
+            new Starter("Chicken Salad","Mix of fresh vegetables and juicy chicken for a healthy start", R.drawable.chickensalad )
+    };
+  private Starter  (String name, String description, int imageID){
+      this.name=name;
+      this.description=description;
+      this.imageID=imageID;
+  }
+
+    public String getName() {return name;}
+
+    public String getDescription() {return description;}
+
+    public int getImageID() {return imageID;}
+}
